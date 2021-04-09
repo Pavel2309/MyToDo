82d6801796c96fa1db65463736b3175355491dd0
Pavel <t329023@gmail.com>
2021-04-09 15:45:37 +0800

    recyclerview implementation

---
+++ app/src/main/java/com/pavel2309/mytodo/adapters/TasksAdapter.kt
@@ -0,0 +1,45 @@
+package com.pavel2309.mytodo.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.pavel2309.mytodo.data.Task
+import com.pavel2309.mytodo.databinding.ItemTaskBinding
+
+class TasksAdapter : ListAdapter<Task, TasksAdapter.TasksViewHolder>(DiffCallback())  {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+        val binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TasksViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class TasksViewHolder(private val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(task: Task) {
+            binding.apply {
+                checkboxCompleted.isChecked = task.completed
+                textViewName.text = task.name
+                textViewName.paint.isStrikeThruText = task.completed
+                labelPriority.isVisible = task.important
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/pavel2309/mytodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/pavel2309/mytodo/ui/tasks/TasksViewModel.kt
@@ -1,6 +1,7 @@
 package com.pavel2309.mytodo.ui.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.pavel2309.mytodo.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,4 +10,7 @@ import javax.inject.Inject
 class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
+
 }
