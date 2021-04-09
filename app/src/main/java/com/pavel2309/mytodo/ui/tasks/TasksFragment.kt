354529523cc256f51cd0ee0c316f9badd51fe218
Pavel <t329023@gmail.com>
2021-04-09 16:54:18 +0800

    search functionality implementation

--- app/src/main/java/com/pavel2309/mytodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/pavel2309/mytodo/ui/tasks/TasksFragment.kt
@@ -1,20 +1,27 @@
 package com.pavel2309.mytodo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.pavel2309.mytodo.R
 import com.pavel2309.mytodo.adapters.TasksAdapter
 import com.pavel2309.mytodo.databinding.FragmentTasksBinding
+import com.pavel2309.mytodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val  viewModel: TasksViewModel by viewModels()
 
+    @ExperimentalCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -32,6 +39,42 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_task, menu)
+
+        val searchItem = menu.findItem(R.id.menu_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_sort_by_name -> {
+                viewModel.sortOrder.value = SortOrder.BY_NAME
+                true
+            }
+            R.id.menu_sort_by_date_created -> {
+                viewModel.sortOrder.value = SortOrder.BY_DATE
+                true
+            }
+            R.id.menu_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                viewModel.hideCompleted.value = item.isChecked
+                true
+            }
+            R.id.meni_delete_all_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
--- app/src/main/java/com/pavel2309/mytodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/pavel2309/mytodo/ui/tasks/TasksViewModel.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.pavel2309.mytodo.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +15,27 @@ class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
 
-    val tasks = taskDao.getTasks().asLiveData()
+    val searchQuery = MutableStateFlow("")
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
 
+    @ExperimentalCoroutinesApi
+    private val tasksFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getTasks(query, sortOrder, hideCompleted)
+    }
+
+    @ExperimentalCoroutinesApi
+    val tasks = tasksFlow.asLiveData()
+
+}
+
+enum class SortOrder {
+    BY_NAME,
+    BY_DATE
 }
