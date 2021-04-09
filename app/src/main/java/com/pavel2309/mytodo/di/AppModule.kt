7b39c813929278b6c208593f35132495e3e0f8a4
Pavel <t329023@gmail.com>
2021-04-09 11:16:24 +0800

    optimization

--- app/src/main/java/com/pavel2309/mytodo/di/AppModule.kt
+++ app/src/main/java/com/pavel2309/mytodo/di/AppModule.kt
@@ -6,14 +6,14 @@ import com.pavel2309.mytodo.data.TaskDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponentManager::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
